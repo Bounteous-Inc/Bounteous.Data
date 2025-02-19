4ba4f2644aa81ca3cfc647f3981696d10eebc8b6
Greg Cook <greg.cook@nutrien.com>
2025-02-19 10:08:08 -0700

    bou-1 - added SyhchronizedOn and IsDelted to AuditImmutableBase

--- src/Bounteous.Data/Domain/IAuditable3.cs
+++ src/Bounteous.Data/Domain/IAuditable3.cs
@@ -6,6 +6,8 @@ public interface IAuditable
     DateTime CreatedOn { get; set; }
     Guid? CreatedBy { get; set; }
     DateTime ModifiedOn { get; set; }
+    DateTime SynchronizedOn { get; set; }
     Guid? ModifiedBy { get; set; }
-    public int Version { get; set; }
+    int Version { get; set; }
+    bool IsDeleted { get; set; }
 }
